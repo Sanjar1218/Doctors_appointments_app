d8c802acacb6d27cf8380af63c2dd21d1dbb2d92
sanjarbek <sanjarsaidov173@gmail.com>
2023-03-31 09:09:06 +0500

    styles added

---
+++ lib/style/main_style.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+ThemeData theme = ThemeData(
+  primarySwatch: Colors.blue,
+  appBarTheme: AppBarTheme(
+    elevation: 0.0,
+    color: Colors.white,
+    titleTextStyle: GoogleFonts.raleway(
+      fontSize: 18.0,
+      fontWeight: FontWeight.w600,
+      color: Colors.black,
+    ),
+  ),
+  textTheme: const TextTheme(
+    titleLarge: TextStyle(color: Colors.black),
+    titleMedium: TextStyle(color: Colors.black),
+    titleSmall: TextStyle(color: Colors.black),
+  ),
+);
